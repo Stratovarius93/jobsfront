03ea962545d5dd5f40be94b7a02d959c9301ae39
dansan99 <danpte1@gmail.com>
2021-01-09 08:37:00 -0500

    2021-01-08

--- lib/login/view/others.dart
+++ lib/login/view/others.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter/services.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 import '../view.dart' show StateMVC, Login;
+import 'package:hexcolor/hexcolor.dart';
 
 class FirstLog extends StatefulWidget {
   const FirstLog({this.title, Key key}) : super(key: key);
@@ -18,48 +20,72 @@ class _FirstLogState extends StateMVC<FirstLog> {
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
-
+    final images = [
+      'assets/images/1.png',
+      'assets/images/2.png',
+      'assets/images/3.png',
+    ];
     return Scaffold(
-      body: Container(
-        height: height,
-        width: width,
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                margin: EdgeInsets.symmetric(vertical: height * .15),
-                width: height * 0.5,
-                height: height * 0.5,
-                child: ListView(
-                  scrollDirection: Axis.horizontal,
-                  children: <Widget>[
-                    Container(
-                      width: height * 0.5,
-                      color: Colors.blue,
+        body: Container(
+      height: height,
+      width: width,
+      color: HexColor('#f5f5f5'),
+      child: SingleChildScrollView(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Container(
+            color: Colors.transparent,
+            margin: EdgeInsets.symmetric(vertical: height * .15),
+            width: height * 0.4,
+            height: height * 0.4,
+            child: new Swiper(
+                itemBuilder: (BuildContext context, int index) {
+                  return ClipRRect(
+                    clipBehavior: Clip.antiAlias,
+                    borderRadius: BorderRadius.circular(35),
+                    child: Image.asset(
+                      images[index],
+                      fit: BoxFit.fill,
                     ),
-                    Container(
-                      width: height * 0.5,
-                      color: Colors.red,
-                    ),
-                    Container(
-                      width: height * 0.5,
-                      color: Colors.green,
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(height: 30),
-              RaisedButton(
-                  onPressed: () {
-                    _firtsLogin(context);
-                  },
-                  child: const Text('Login', style: TextStyle(fontSize: 20))),
-            ],
+                  );
+                },
+                itemCount: images.length,
+                scale: 0.8,
+                autoplay: true,
+                autoplayDelay: 10000,
+                duration: 500),
           ),
-        ),
+          Container(
+            width: height * 0.2,
+            child: Form(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  TextFormField(
+                    inputFormatters: <TextInputFormatter>[
+                      FilteringTextInputFormatter.digitsOnly
+                    ],
+                    keyboardType: TextInputType.number,
+                    decoration: const InputDecoration(
+                      hintText: '(819) 740-0000',
+                    ),
+                    validator: (value) {
+                      return 'Please enter your number';
+                    },
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 16.0),
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text('Login'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ]),
       ),
-    );
+    ));
   }
 }
 
