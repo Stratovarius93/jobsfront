1f6c3078326aaf1a312138d0c4cad45773836657
dansan99 <danpte1@gmail.com>
2021-01-16 09:21:20 -0500

    2021-01-16

--- lib/login/view/register.dart
+++ lib/login/view/register.dart
@@ -5,6 +5,8 @@ import 'package:hexcolor/hexcolor.dart';
 import '../controller.dart' show verificarAuthCode;
 import '../model.dart' show VerificarAuthResponse, VerificarNumeroResponse;
 
+import 'package:form_validator/form_validator.dart';
+
 class Registro extends StatefulWidget {
   const Registro({this.title, Key key}) : super(key: key);
   final String title;
@@ -14,11 +16,16 @@ class Registro extends StatefulWidget {
 }
 
 class _RegistroState extends StateMVC<Registro> {
+  GlobalKey<FormState> _form = GlobalKey<FormState>();
+
   @override
   void initState() {
     super.initState();
+    _form.currentState.validate();
   }
 
+  final validator1 = ValidationBuilder().maxLength(50).build();
+  final validator2 = ValidationBuilder().maxLength(59).build();
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
@@ -28,9 +35,20 @@ class _RegistroState extends StateMVC<Registro> {
             body: Container(
                 width: width,
                 color: HexColor('#f5f5f5'),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [Form()],
+                child: SingleChildScrollView(
+                  child: Form(
+                      key: _form,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          TextFormField(
+                            validator: validator1,
+                            decoration: const InputDecoration(
+                              hintText: 'Name',
+                            ),
+                          )
+                        ],
+                      )),
                 ))));
   }
 }
